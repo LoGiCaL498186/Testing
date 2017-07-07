02a99eea013195068f2deb55398216e4f2bfc6e9
dave498186 <dave498186@gmail.com>
2017-07-06 22:00:40 -0400

    first commit

---
+++ RakeFile.rb
@@ -0,0 +1,23 @@
+task :step_1 do
+  puts "TEST STEP 1"
+end
+
+task :step_2 do
+  puts "TEST STEP 2"
+end
+
+task :step_3 do
+  puts "TEST STEP 3"
+end
+
+task :step_4 do
+  puts "TEST STEP 4"
+end
+
+task :step_5 do
+  puts "TEST STEP 5"
+end
+
+task :step_6 => [:step_1, :step_2, :step_3, :step_4, :step_5] do
+  puts "TEST STEP 6"
+end
